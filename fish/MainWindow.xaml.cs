5d34903f423470443c251cc5edcb90f2706afb0b
quanzhe <clqqz123@126.com>
2018-12-18 17:15:15 +0800

    第一个界面完成

--- fish/MainWindow.xaml.cs
+++ fish/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -22,6 +23,9 @@ namespace fish
         public MainWindow()
         {
             InitializeComponent();
+            workspaceScoll.HorizontalScrollBarVisibility = ScrollBarVisibility.Hidden;
+            workspaceScoll.VerticalScrollBarVisibility = ScrollBarVisibility.Hidden;
+            LoadWorkSpaceImages();
         }
 
         private void Move_MouseMove(object sender, MouseEventArgs e)
@@ -30,6 +34,70 @@ namespace fish
             {
                 this.DragMove();
             }
+        }
+
+        private string[] recursed(string path, string[] patterns)
+        {
+            string[] arrList = new string[0];
+            foreach (string str in patterns)
+            {
+                string[] list = Directory.GetFiles(path, str, SearchOption.AllDirectories);
+                if (list != null)
+                {
+                    string[] temp = arrList;
+                    arrList = new string[arrList.Length + list.Length];
+                    Array.Copy(temp, 0, arrList, 0, temp.Length);
+                    Array.Copy(list, 0, arrList, temp.Length, list.Length);
+                }
+            }
+            return arrList;
+        }
+
+        private void InitRows(int rowCount, Grid g)
+        {
+            while (rowCount-- > 0)
+            {
+                RowDefinition rd = new RowDefinition();
+                rd.Height = new GridLength(170);
+                g.RowDefinitions.Add(rd);
+            }
+        }
+        private void InitColumns(int colCount, Grid g)
+        {
+            while (colCount-- > 0)
+            {
+                ColumnDefinition rd = new ColumnDefinition();
+                rd.Width = new GridLength(180);
+                g.ColumnDefinitions.Add(rd);
+            }
+        }
+
+        private void LoadWorkSpaceImages()
+        {
+            string[] list = this.recursed(@"D://WorkSpace", new string[] { "*.jpg", "*.png", "*.gif" });
+            InitRows(list.Length/3+1,workspaceGrid);
+            InitColumns(3,workspaceGrid);
+            for (int i = 0;i < list.Length;i++)
+            {
+                workSpaceUnit workSpaceUnit = 
+                    new workSpaceUnit(list[i].Split('\\')[1], 
+                    new BitmapImage(new Uri(list[i],UriKind.Absolute)));
+                workspaceGrid.Children.Add(workSpaceUnit);
+                workSpaceUnit.MouseLeftButtonUp += WorkSpaceUnit_MouseLeftButtonUp;
+                Grid.SetColumn(workSpaceUnit, i % 3);
+                Grid.SetRow(workSpaceUnit, i / 3);
+            }
+        }
+
+        private void WorkSpaceUnit_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            workSpaceUnit workSpaceUnit = sender as workSpaceUnit;
+            MessageBox.Show(workSpaceUnit.textBlock.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- fish/Properties/Resources.Designer.cs
+++ fish/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace fish.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap xianyu {
+            get {
+                object obj = ResourceManager.GetObject("xianyu", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
